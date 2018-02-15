4f05b86db24306911c48a3ef84edcbc9d737631b
QBielka <qbielka@sfu.ca>
2018-02-14 16:38:52 -0800

    Board Logic

---
+++ app/src/main/java/com/sosy/qbielka/loveseeker/HeartSeeker.java
@@ -0,0 +1,13 @@
+package com.sosy.qbielka.loveseeker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeartSeeker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heart_seeker);
+    }
+}
---
+++ app/src/main/java/com/sosy/qbielka/loveseeker/Tile.java
@@ -0,0 +1,10 @@
+package com.sosy.qbielka.loveseeker;
+
+/**
+ * Created by Quince Bielka on 2018-02-14.
+ * Holds a state of the mine
+ */
+
+public enum Tile {
+    HEART, BLANK
+}
