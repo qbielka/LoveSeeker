aab951c0dcc115efc08a4032aaab2065e0c9aa6a
lkoo <lkoo@sfu.ca>
2018-02-16 08:57:01 -0800

    OptionsMenu.java page created

---
+++ app/src/main/java/com/sosy/qbielka/loveseeker/OptionsMenu.java
@@ -0,0 +1,17 @@
+package com.sosy.qbielka.loveseeker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OptionsMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options_menu);
+    }
+
+    //TODO: create radio buttons for options choices
+
+    //TODO: implement Singleton class Options.java for saving information between sessions purposes
+}
