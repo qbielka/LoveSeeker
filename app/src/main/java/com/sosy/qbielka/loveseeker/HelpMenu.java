15f8c5f2d5f589070cce9cff3b62ddead2649c5a
QBielka <qbielka@sfu.ca>
2018-02-16 12:17:16 -0800

    Options LayoutMenu

--- app/src/main/java/com/sosy/qbielka/loveseeker/HelpMenu.java
+++ app/src/main/java/com/sosy/qbielka/loveseeker/HelpMenu.java
@@ -1,7 +1,11 @@
 package com.sosy.qbielka.loveseeker;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpMenu extends AppCompatActivity {
 
@@ -11,5 +15,21 @@ public class HelpMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_menu);
+
+        backBtn();
+    }
+
+    private void backBtn() {
+        Button back = (Button) findViewById(R.id.help_back_btn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, HelpMenu.class);
     }
 }
--- app/src/main/java/com/sosy/qbielka/loveseeker/WelcomeScreen.java
+++ app/src/main/java/com/sosy/qbielka/loveseeker/WelcomeScreen.java
@@ -1,5 +1,6 @@
 package com.sosy.qbielka.loveseeker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        startActivity(new Intent(WelcomeScreen.this, OptionsMenu.class));
     }
 }
