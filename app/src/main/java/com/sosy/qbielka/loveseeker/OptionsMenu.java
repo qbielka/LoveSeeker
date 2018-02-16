6c86ecb7d5b9fa9e63c9efe473d8d9337e875031
lkoo <lkoo@sfu.ca>
2018-02-16 13:55:42 -0800

    very basic work on optionsMenu

--- app/src/main/java/com/sosy/qbielka/loveseeker/OptionsMenu.java
+++ app/src/main/java/com/sosy/qbielka/loveseeker/OptionsMenu.java
@@ -2,18 +2,40 @@ package com.sosy.qbielka.loveseeker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class OptionsMenu extends AppCompatActivity {
+
+    private Options loadCurrentOptions;
+
+    private int[] optionsLoveNums = {6, 10, 15, 20};
+
+    private int[] optionsRowsNums = {4, 5, 6};
+    private int[] optionsColsNums = {6, 10, 15};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_menu);
+
+        //TODO: implement Singleton class Options.java for saving information between sessions purposes
+        loadCurrentOptions = Options.getInstance();
+
+        createOptionMenuLoveNum();
+        createOptionMenuBoardSize();
     }
 
     //TODO: create radio buttons for options choices
 
-    //TODO: implement Singleton class Options.java for saving information between sessions purposes
+    private void createOptionMenuLoveNum() {
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroupLoveNum);
+
+
+    }
+
+    private void createOptionMenuBoardSize() {
+
+    }
 
     //TODO: press back to return to main menu
 }
