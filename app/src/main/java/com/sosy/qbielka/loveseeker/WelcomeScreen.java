be931fe707c3e2fa4231e1cc16bfa10956395d6c
QBielka <qbielka@sfu.ca>
2018-02-17 12:45:20 -0800

    adds auto advance in Welcome screen

--- app/src/main/java/com/sosy/qbielka/loveseeker/WelcomeScreen.java
+++ app/src/main/java/com/sosy/qbielka/loveseeker/WelcomeScreen.java
@@ -1,35 +1,62 @@
 package com.sosy.qbielka.loveseeker;
 
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import static java.util.concurrent.Executors.*;
+
 public class WelcomeScreen extends AppCompatActivity {
 
+    private static final int MILLISECOND_WAIT = 5000;
+    private final boolean[] skipped = {false};
+    private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-        final boolean[] skipped = {false};
         // welcome screen code
         final Button skip = (Button) findViewById(R.id.skip_btn);
 
-        skipBtn(skipped, skip);
+        skipBtn(skip);
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean b) {
+        super.onWindowFocusChanged(b);
+        handler=new Handler();
+        Runnable r=new Runnable() {
+            public void run() {
+                autoAdvance();
+            }
+        };
+        handler.postDelayed(r, MILLISECOND_WAIT);
+    }
+
+    private void autoAdvance() {
         if(!skipped[0]) {
             bootMenuAndTerminate();
+            skipped[0] = true;
         }
     }
 
-    private void skipBtn(final boolean[] skipped, Button skip) {
+    private void skipBtn(Button skip) {
         skip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                skipped[0] = true;
-                bootMenuAndTerminate();
+                if(!skipped[0]) {
+                    bootMenuAndTerminate();
+                    skipped[0] = true;
+                }
             }
         });
     }
@@ -38,6 +65,4 @@ public class WelcomeScreen extends AppCompatActivity {
         startActivity(new Intent(WelcomeScreen.this, MainMenu.class));
         finish();
     }
-
-    //TODO: Create welcoming animation with skip option available as a button.
 }
