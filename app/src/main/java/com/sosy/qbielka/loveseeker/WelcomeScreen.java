a82ab73f2b62505c8fbc89f20599a22dbd19218a
QBielka <qbielka@sfu.ca>
2018-02-16 18:46:02 -0800

    null for pull

--- app/src/main/java/com/sosy/qbielka/loveseeker/WelcomeScreen.java
+++ app/src/main/java/com/sosy/qbielka/loveseeker/WelcomeScreen.java
@@ -1,6 +1,7 @@
 package com.sosy.qbielka.loveseeker;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
